4aa1076bc26beb70d9d5e8e5c9bb376173b09789
munroe1786 <era3munroe@gmail.com>
2021-08-02 22:37:58 -0500

    add suits and values as global variables

--- deck.js
+++ deck.js
@@ -1,3 +1,20 @@
+const SUITS = ["♠", "♣", "♥", "♦"]
+const VALUES = [
+    "A", 
+    "2", 
+    "3", 
+    "4", 
+    "5", 
+    "6", 
+    "7", 
+    "8", 
+    "9", 
+    "10", 
+    "J", 
+    "Q", 
+    "K"
+]
+
 class Deck {
     constructor(cards) {
         this.cards = cards
