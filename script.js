ef959bd90a9078aa95b56d2618e9ee64e8e1e971
munroe1786 <era3munroe@gmail.com>
2021-08-13 22:00:46 -0500

    add startGame function to script.js and delete console.log of deck.cards

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ import Deck from "./deck.js";
 
 const computerCardSlot = document.querySelector(".computer-card-slot")
 
-const deck = new Deck()
-deck.shuffle()
-console.log(deck.cards)
+let playerDeck, computerDeck
+
+startGame()
+function startGame() {
+    const deck = new Deck()
+    deck.shuffle()  
+
+    const deckMidpoint = Math.cell(deck.numberOfCards / 2)
+    playerDeck = new Deck(deck.cards.slice(0, deckMidpoint))
+    computerDeck
+}
+
 
 computerCardSlot.appendChild(deck.cards[0].getHTML())
